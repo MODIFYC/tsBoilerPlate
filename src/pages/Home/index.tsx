e0ce924c6ccfb0628d965a19740bf5ec9482ef6f
MODIFYC <chsj5742@naver.com>
2024-05-07 21:08:13 +0900

    directory settings

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+//import { useQuery } from "react-query";
+import Button from "../../components/common/Button";
+
+function Home() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default Home;
